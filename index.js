adebac47f7cbfc72aa4e388529f7dbce307c1626
Lucas <lucaswoodyard@yahoo.com>
2022-08-08 15:36:14 -0500

    commit tree c18f79b4be81e9ef0ce1b7a99a956865ab42b715

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    console.log('Hello');
+    const event = document.querySelector('p')
+    event.textContent = 'This is really cool!';
+    
+})
+
+console.log('Hello');
